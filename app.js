16572bb846020f0c08e0bd42980f02d606701670
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-05-29 23:01:27 +0500

    this is the practice of java

---
+++ app.js
@@ -0,0 +1,114 @@
+// //SPREAD OPERITER CODE 
+// let arr = [56, 565 ,'kirshanan',666, 444, 45, 554,44,44]
+// let arr2 =[ 1,2,3,4,5,65,678,8,9,,89,45,8,678,45,678,35,]
+// let arr3 =[ ...arr, ...arr2]
+// document.write(arr3)
+
+// let obj = {
+//     first :'one',
+//     last :'two',
+//     end :'three'
+// }
+// let obj2 = {
+//     ...obj ,
+//     lname :'kiehsnaa'
+// }
+// console.log(obj2)
+
+
+// even or odd program
+
+// for (var i = 1; <= 50; i++){
+//     i
+// }
+// var a = 3
+// var b = 4 
+// var c = 5 
+// var d = 1 
+// var eq = a-- + b-- + ++c  + d  + a++  - b
+//         //  3 +  4  +  6  +  1   +  3
+//         document.write(eq)
+// var a = 2
+// var b = 5
+// var eq = ++b + a++ + a++ + b++ + ++a
+//         //   6  +   2 +  3  +  6  +  4
+//         document.write(eq)
+// var a = 5
+// var b = 3 
+// var c = 6
+// var eq =  b-- + ++a + c++ + a
+//         //   3  +  6  +  6   + 5
+//         document.write(eq)
+
+//EVEN AND ODD NUMBER 
+// for (var i= 1; i<=50 ; i++){
+//     if ( i%2  !== 0){
+//         document.write(i, "<br>")
+//     }
+// }
+// var userInp = +prompt('Enter Value Number ',5)
+// var kahanTak = +prompt('Enter kahan tak ka number',10)
+// for (var i= 1; i <=kahanTak; i++){
+//     // document.write(userInp,"<br>")
+//      document.write(`${userInp} x ${i} =${userInp * i } <br >`)
+// }
+
+// var userInp = +prompt('enter the value ',2)
+// var endoftheGame = +prompt('enter the lastest value as you still find',10)
+// for (var i= 1; i<= endoftheGame; i++ ){
+//     document.write(`${userInp}x ${i} =${userInp*i } <br>`)
+// }
+// var userInp =+prompt('ENTER THE VALUE',5)
+// var justLast = +prompt('Enter the last value as you still find',10)
+// for (var i=1; i<=justLast; i++){
+//     document.write(`${userInp}x ${i} =${userInp*i} <br>`)
+// }
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[1])
+
+//COLOR CHANGING FUNCTION 
+// console.log(getElements)
+
+
+
+
+// //SPREAD OPERITER CODE 
+// let arr = [56, 565 ,'kirshanan',666, 444, 45, 554,44,44]
+// let arr2 =[ 1,2,3,4,5,65,678,8,9,,89,45,8,678,45,678,35,]
+// let arr3 =[ ...arr, ...arr2]
+// document.write(arr3)
+
+// let obj = {
+//     first :'one',
+//     last :'two',
+//     end :'three'
+// }
+// let obj2 = {
+//     ...obj ,
+//     lname :'kiehsnaa'
+// }
+// console.log(obj2)
+// function colorChange(){
+//     let getElements = document.getElementsByTagName('p')
+//  for (let i =0; i<getElements.length; i++){
+//     getElements[i].style.backgroundColor ='black';
+//     getElements[i].style.color = 'white'
+//  }
+// } 
+
+  //promise function 
+  let pro = new Promise((res, rej)=>{
+   let khilo ='pizza khawogai'
+   if (khilo == 'piazza khawogai'){
+      res ('han bhai zarror khawnogaa')
+   }
+   else{
+      res('nhi khana muji bhai ')
+   }
+  })
+  pro.then((data)=>{
+   document.write (data)
+  })
+.catch((err)=>{
+   console.log(err)
+})
