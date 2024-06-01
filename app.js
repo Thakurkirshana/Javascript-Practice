cdf5fc200ca0c1536c29dee6dfb29ed4dfe7df85
Kirshan Thakur <166369674+Thakurkirshana@users.noreply.github.com>
2024-06-01 11:27:57 +0500

    Quiz app

--- app.js
+++ app.js
@@ -160,20 +160,20 @@
 // API men deta hamisha array of Object ki form men hota hai 
 
 // JSON javascript object Notition ki form men hota hai 
-  let pro = new Promise((res,rej) =>{
-   let hanbhai ='kase ho'
-if (hanbhai== 'kase ho bhai'){
-   res('han bhai theek hai')
-}
-else{
-   rej('nhi hai bahi ')
-}
-  })
-pro.then((data)=>{
-   console.log(data)
-}).catch((err )=>{
-   console.log(err)
-})
+//   let pro = new Promise((res,rej) =>{
+//    let hanbhai ='kase ho'
+// if (hanbhai== 'kase ho bhai'){
+//    res('han bhai theek hai')
+// }
+// else{
+//    rej('nhi hai bahi ')
+// }
+//   })
+// pro.then((data)=>{
+//    console.log(data)
+// }).catch((err )=>{
+//    console.log(err)
+// })
 // let pro = new Promice((res,rej) => {
 //    let wordCup ="pakistan"
 //     if(WordCup =="PAKISTAN"){
@@ -186,3 +186,237 @@ pro.then((data)=>{
 //  .catch((err)=>{
 //  console.log(err)})
               
+
+
+
+
+
+
+
+
+
+var questions = [
+   {
+       question: 'What does HTML stand for?',
+       option1: 'Hyperlinks and Text Markup Language',
+       option2: 'Hypertext Markup Language',
+       option3: 'Home Tool Markup Language',
+       correctOption: "Hypertext Markup Language"
+   },
+   {
+       question: 'Who is making the Web standards?',
+       option1: 'Google',
+       option2: 'The World Wide Web Consortium',
+       option3: 'Microsoft',
+       correctOption: "The World Wide Web Consortium"
+   },
+   {
+       question: 'Choose the correct HTML element for the largest heading:',
+       option1: '<heading>',
+       option2: '<h6>',
+       option3: '<h1>',
+       correctOption: "<h1>"
+   },
+   {
+       question: 'What is the correct HTML element for inserting a line break?',
+       option1: '<linebreak>',
+       option2: '<br>',
+       option3: '<break>',
+       correctOption: "<br>"
+   },
+   {
+       question: 'What is the correct HTML for adding a background color?',
+       option1: '<body bg="yellow">',
+       option2: '<background>yellow</background>',
+       option3: '<body style="background-color:yellow;">',
+       correctOption: '<body style="background-color:yellow;">'
+   },
+   {
+       question: 'Choose the correct HTML element to define important text:',
+       option1: '<strong>',
+       option2: '<b>',
+       option3: '<i>',
+       correctOption: '<strong>'
+   },
+   {
+       question: 'Choose the correct HTML element to define emphasized text:',
+       option1: '<italic>',
+       option2: '<i>',
+       option3: '<em>',
+       correctOption: "<em>"
+   },
+   {
+       question: 'What is the correct HTML for creating a hyperlink?',
+       option1: '<a>http://www.w3schools.com</a>',
+       option2: '<a href="http://www.w3schools.com">W3Schools</a>',
+       option3: '<a url="http://www.w3schools.com">W3Schools.com</a>',
+       correctOption: '<a href="http://www.w3schools.com">W3Schools</a>'
+   },
+   {
+       question: 'Which character is used to indicate an end tag?',
+       option1: '*',
+       option2: '/',
+       option3: '<',
+       correctOption: "/"
+   },
+   {
+       question: 'How can you open a link in a new tab/browser window?',
+       option1: '<a href="url" target="new">',
+       option2: '<a href="url" new>',
+       option3: '<a href="url" target="_blank">',
+       correctOption: '<a href="url" target="_blank">'
+   },
+   {
+       question: 'Which of these elements are all <table> elements?',
+       option1: '<table> <tr> <td>',
+       option2: '<table> <head> <tfoot>',
+       option3: '<table> <tr> <tt>',
+       correctOption: "<table> <tr> <td>"
+   }
+]
+
+
+var ques = document.getElementById('ques')
+var opt1 = document.getElementById('opt1')
+var opt2 = document.getElementById('opt2')
+var opt3 = document.getElementById('opt3')
+var index = 0
+var btn = document.getElementById('btn')
+ var score = 0
+ var min = 1
+ var sec = 59 
+ 
+// timer = document.getElementById('timer')
+//  var interval = setInterval(function(){
+//  timer.innerHTML = `${min} : ${sec}`
+//  sec--
+//  if (sec <0){
+//    min--
+//    sec = 59
+//    if(min<0){
+//       min = 1
+//       sec =59
+//       nextQuestion()
+//    }
+//  }
+//  },100)
+// 
+
+function nextQuestion(){
+var getOptions = document.getElementsByName('options')
+  for (var i= 0 ; i<getOptions.length; i++){
+
+if(getOptions[i].checked){
+var selectedValue = getOptions[i].value
+var selectedQues = questions[index -1 ]['question']
+var selectedAns  =  questions [index -1 ][`option${selectedValue}`]
+var correctOption = questions[index -1 ][`correctOption`]
+if(selectedAns == correctOption){
+ score++
+}
+console.log(selectedAns)
+
+// console.log(selectedQues)
+//  console.log(getOptions[i].value)  
+}
+
+   // var selectedValue = getOptions[i].value
+   // var selectedQues = questions[index]
+   // console.log (selectedQues)
+   // // console.log(getOptions[i].value)
+   // console.log(getOptions[i])
+   getOptions[i].checked = false
+  }
+btn.disabled = true
+  
+   if(index > questions.length -1 ){
+      document.write('Your Percentage is ' +((score / questions.length)*100).toFixed(2))
+   }
+   else{
+      ques.innerText = questions[index].question
+      opt1.innerText = questions[index].option1
+      opt2 .innerText = questions[index].option2
+      opt3.innerText = questions[index].option3
+      index++
+   }
+  
+}
+nextQuestion()
+
+function clicked(){
+  
+   btn.disabled = false
+}
+// var ques = document.getElementById('ques')
+// var opt1 = document.getElementById('opt1')
+// var opt2 = document.getElementById('opt2')
+// var opt3 = document.getElementById('opt3')
+
+// var index = 0
+// var btn = document.getElementById('btn')
+
+
+
+
+// function nextQuestion (){
+
+//      var getOptions = document.getElementsByClassName('options')
+//       for ( var i=0; i<getOptions.length; i++ ){
+//  var selectedValue = getOptions[i].value 
+
+// //   console.log(getOptions)
+//       getOptions[i].checked = false
+
+// }
+
+
+//  btn.disabled = true
+
+//    if (index > questions.length - 1){
+//        console.log('THE END QUESTIONS')
+//    }
+// else{
+//    ques.innerText= questions [index].question
+//    opt1.innerText= questions[index].option1
+//    opt2.innerText= questions[index].option2
+//    opt3.innerText= questions[index].option3
+//   index++ 
+
+// }
+// }
+// nextQuestion()
+
+
+
+// function clicked(){
+   
+//    btn.disabled = false
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
